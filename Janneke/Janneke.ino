e88543ea35b45a7847b25be41147ceb62a28f424
Hopmaxx <gitkraken@jannekehopman.com>
2020-05-10 16:33:19 +0200

    Buttons light up when pressed
    Function added so leds are on when button is pressed.

--- Janneke/Janneke.ino
+++ Janneke/Janneke.ino
@@ -6,11 +6,7 @@
 #define cLedSpeed 50
 
 const uint8_t cLedPins[cLedAmnt] = {12,11,10,9,8,7,A5,2,3,4,5,6};
-const uint8_t cRowId[cRowAmnt] = {63,31,21,15,12,10};
-
-int topRow = 0;
-int bottomRow = 0;
-int buttonPressed = 0; 
+const uint8_t cRowId[cRowAmnt] = {63,31,21,15,12,10}; 
 
 void setup() {
     
@@ -27,40 +23,49 @@ void setup() {
 
 
 void loop() {
-
+  /*
   for(int i = 0; i < cLedAmnt; i++){
     digitalWrite(cLedPins[i],HIGH);
     delay(cLedSpeed);
     digitalWrite(cLedPins[i],LOW);
     delay(cLedSpeed);
   }
-  
-  topRow = analogRead(cBtnUpPin) / 16;
-  bottomRow = analogRead(cBtnDownPin) / 16;
-  
-  buttonPressed = 0;
-  for(int i = 0; i < cRowAmnt; i++){
-    if(topRow == cRowId[i]){
-      buttonPressed = i+1;
-      break;
-    }//if
-  }//for  
+  */  
+  int topRow = analogRead(cBtnUpPin) / 16;
+  int bottomRow = analogRead(cBtnDownPin) / 16;
 
-  Serial.println(getButtonId(topRow, cRowId));
+  topRow = getButtonId(topRow, cRowId, cRowAmnt);
+  bottomRow = getButtonId(bottomRow, cRowId, cRowAmnt) + 6 * bool(bottomRow);
+
+  if(topRow > 0 || bottomRow > 0){
+    digitalWrite(cLedPins[topRow-1],HIGH);
+    digitalWrite(cLedPins[bottomRow
+    -1],HIGH);    
+  }else{
+    for(int i = i; i < cLedAmnt; i++){
+      digitalWrite(cLedPins[i],LOW);   
+    }
+  }
+
+
+  
+  Serial.print(topRow);
+  Serial.print(" ");
+  Serial.println(bottomRow);
   delay(50);
 }//loop
 
-int getButtonId(int rawValue, uint8_t * rowId){
+int getButtonId(int rawValue, uint8_t * compareArray, int compareArraySize){
   
   int buttonId = 0;
 
-  for(int i = 0; i < sizeof(rowId); i++){
-    if(rawValue == rowId[i]){
+  for(int i = 0; i < compareArraySize; i++){
+    if(rawValue == compareArray[i]){
       buttonId = i+1;
       break;
     }//if
   }//for 
 
-  return sizeof(rowId[2]); 
+  return buttonId;
 }
 
